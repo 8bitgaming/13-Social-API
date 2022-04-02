63f45ce9153e6b76a605b38df533a77fbbf1cb0f
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-04-02 01:16:08 -0500

    Added controllers and routes for all user endpoints and tested

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-// const pizzaRoutes = require('./pizza-routes');
+const userRoutes = require('./user-routes');
+// const thoughtRoutes = require('./thought-routes');
 
-// // add prefix of `/pizzas` to routes created in `pizza-routes.js`
-// router.use('/pizzas', pizzaRoutes);
+router.use('/users', userRoutes);
+// router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
