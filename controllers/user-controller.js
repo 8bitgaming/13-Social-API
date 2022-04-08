0e310d599ce8682a9c52771b43575b5215d4f6f2
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-04-08 00:47:44 -0500

    fixing reactions

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,6 +4,15 @@ const userController = {
     // get all users
     getAllUsers(req, res) {
         User.find({})
+        .populate({
+          path: 'thoughts',
+          select: '-__v'
+        })
+        .populate({
+          path: 'friends',
+          select: '-__v'
+        })
+        .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
